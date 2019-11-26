c6ee0a002245a7260d52cd1aea5e96cc8528f7b2
emmanuelchacon <emmanuelchacon22@gmail.com>
2019-11-25 21:46:39 -0400

    024 -Controlador.- Se creo la Clase controlador y se programo cada una de las funciones de esta, y se creo rutas para cada una.
    Co-Authored-By: null <emmanuelchacon@users.noreply.github.com>

---
+++ app/Http/Controllers/CategoriaController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Categoria; //Importamos nuestro modelo Categoria
+
+
+class CategoriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $categorias = Categoria::all(); //Una vez que importamos la clase Categoria utilizamos el elmetodo all() y le asignamos todos los datos a la variable $categoria
+
+        // echo "<pre>";
+        // var_dump($categorias);
+        // echo "</pre>";
+        return $categorias;
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) //Request recibira datos desde  un formulario y lo asignara a cada campo de la tabla categorias
+    {
+        $categoria = new Categoria();
+
+        $categoria->nombre = $request->nombre;
+        $categoria->descripcion = $request->descripcion;
+        $categoria->condicion = '1';
+
+        $categoria->save();
+    }
+
+
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+      $categoria = Categoria::findOrFail($request->id); //Buscamos el id del registro que se actualizara
+
+      $categoria->nombre = $request->nombre;
+      $categoria->descripcion = $request->descripcion;
+      $categoria->condicion = '1';
+
+      $categoria->save();
+    }
+
+    public function desactivar(Request $request)
+    {
+      $categoria = Categoria::findOrFail($request->id); //Buscamos el id del registro que se desactivara
+
+      $categoria->condicion = '0';
+
+      $categoria->save();
+    }
+
+    public function activar(Request $request)
+    {
+      $categoria = Categoria::findOrFail($request->id); //Buscamos el id del registro que se activara
+
+      $categoria->condicion = '1';
+
+      $categoria->save();
+    }
+
+
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('contenido/contenido_main');
 });
+
+
+Route::get('/categoria','CategoriaController@index');
+
+Route::post('/categoria/registrar','CategoriaController@store');
+
+Route::put('/categoria/actualizar','CategoriaController@update');
+
+Route::put('/categoria/desactivar','CategoriaController@desactivar');
+
+Route::put('/categoria/activar','CategoriaController@activar');
